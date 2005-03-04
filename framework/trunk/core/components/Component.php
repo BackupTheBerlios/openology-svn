6dda0bc38351b0afb0e7347081b78e0d335ebaa3
kenchew <kenchew@a0a65062-33ee-0310-9b6a-bdba8ff39ab1>
2005-03-04 09:31:17 +0000

    Added Components to frameworka

---
+++ framework/trunk/core/components/Component.php
@@ -0,0 +1,48 @@
+<?php
+// +---------------------------------------------------------------------------+
+// | This file is part of the Openology FrameWork                              |
+// | Copyright (c) 2004 Openology Pte Ltd                                      |
+// |                                                                           |
+// | For the full copyright and license information, please view the COPYRIGHT |
+// | file that was distributed with this source code. If the COPYRIGHT file is |
+// | missing, please visit Openology homepage: http://www.openology.org/       |
+// +---------------------------------------------------------------------------+ 
+//
+// Created on 2005-1-25 17:22:04
+// $Id:$ 
+
+/**
+ * Base class for component. All form elements extends from this class. 
+ *
+ * @package openology
+ * @subpackage components
+ * @author Ken Chew <ken.chew@openology.com>
+ * @author Andy Ma  <andy.ma@openology.com>
+ * @copyright (c) 2004 Openology Pte Ltd
+ */
+/**
+ * Base class for form component. All components extends from this class. 
+ *
+ * @package openology.components
+ */
+
+class Component
+{    
+    /**
+     * 
+     * @var String
+     */
+    var $id;
+    
+    /**
+     * Constructor
+     * 
+     * @return  void
+     */
+    function Component()
+    {
+        
+    }    
+    
+}
+?>
---
+++ framework/trunk/core/components/ComponentContainer.php
@@ -0,0 +1,115 @@
+<?php 
+// +---------------------------------------------------------------------------+
+// | This file is part of the Openology FrameWork                              |
+// | Copyright (c) 2004 Openology Pte Ltd                                      |
+// |                                                                           |
+// | For the full copyright and license information, please view the COPYRIGHT |
+// | file that was distributed with this source code. If the COPYRIGHT file is |
+// | missing, please visit Openology homepage: http://www.openology.org/       |
+// +---------------------------------------------------------------------------+ 
+//
+// Created on 2005-1-26 15:52:44
+// $Id: Form.php 275 2005-01-25 09:01:09Z ken $
+/**
+ * Component Container. 
+ *
+ * @package openology
+ * @subpackage components
+ * @author Ken Chew <ken.chew@openology.com>
+ * @author Andy Ma  <andy.ma@openology.com>
+ * @copyright (c) 2004 Openology Pte Ltd
+ */
+
+/**
+ * Component Container. 
+ *
+ * @package openology.components
+ */
+class ComponentContainer
+{
+    /**
+     * @var array
+     */
+    var $arr_element = array ();
+
+    /**
+     * @var array
+     */
+    var $arr_attr = array ();
+
+    /**
+     * Constructor
+     * 
+     * @param   String $name
+     * @param   String $action
+     * @param   String $method
+     * @return  void
+     */
+    function ComponentContainer()
+    {
+
+    }
+
+    /**
+     * set the attribute of the ComponentContainer
+     * 
+     * @param   string $name
+     * @param   string $value
+     * @return  void
+     */
+    function setAttribute($name, $value)
+    {
+        $this->arr_attr[$name] = $value;
+    }
+
+    /**
+     * get the attribute of the ComponentContainer
+     * 
+     * @param   string $name
+     * @return  void
+     */
+    function getAttribute($name)
+    {
+        return $this->arr_attr[$name];
+    }
+
+    /**
+    * Adds an element into the form
+    *
+    * @param    String      $type       
+    * @return   object     reference to element
+    */
+    function & addElement($type)
+    {
+        $obj_element = & $this->_loadElement($type);
+
+        $this->arr_element[] = & $obj_element;
+
+        return $obj_element;
+    }
+
+    /**
+     * Returns a form element of the given type
+     *
+     * @param     string   $type    element type
+     * @return    object    a new element
+     * 
+     */
+    function & _loadElement($type)
+    {
+        $type = ucfirst(strtolower($type));
+        if (!file_exists(OOO_CORE.'/components/'.$type.'.php'))
+        {
+            echo 'This type of component ('.$type.') does not exist!';
+            // TODO: Isolate message to language file.
+            exit;
+        }
+        include_once OOO_CORE.'/components/'.$type.'.php';
+
+        $classname = $type;
+
+        $obj_element = & new $classname ();
+        return $obj_element;
+    }    
+}
+?>
